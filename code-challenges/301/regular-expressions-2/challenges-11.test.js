58482b56e25253a18667160b836ba5a0af5372ba
Jacob Anderson <1anderson2jacob@gmail.com>
2018-11-29 17:49:41 -0800

    solved throught 2

--- code-challenges/301/regular-expressions-2/challenges-11.test.js
+++ code-challenges/301/regular-expressions-2/challenges-11.test.js
@@ -34,9 +34,9 @@ Note: if you ever need to validate an email using a regex in practice, the Inter
 ------------------------------------------------------------------------------------------------ */
 
 const validateEmail = (email) => {
-  // Solution code here...
+  const regx = /(.*?)@(.*?)\.(com|net|org)/g
+  return regx.test(email)
 };
-
 /* ------------------------------------------------------------------------------------------------
 CHALLENGE 3
 
